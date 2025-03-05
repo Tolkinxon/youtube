20d6bd815cdcf3a305cb56cc84db1fdcb4b0ae01
Soliyev Tolkinxon <krosstony639@gmail.com>
2025-03-05 20:30:07 +0500

    Finish all

--- delete.js
+++ delete.js
@@ -10,9 +10,12 @@ const [ , ,idx] = process.argv
         let users = await fs.readFile(path.join(__dirname, "users.json"), 'utf8');
         users = users ? JSON.parse(users) : [];
 
+        // id bo'yicha
         // const index = users.findIndex(item => item.id == idx);
+        // if(index == -1) return console.log("ClientError: Wrong id inserted!");
         // users.splice(index, 1);
 
+        if(users.length <= idx) return console.log("ClientError: Wrong index inserted!"); 
         users.splice(idx, 1);
 
         fs.writeFile(path.join(__dirname, "users.json"), JSON.stringify(users, null, 4));
--- update.js
+++ update.js
@@ -1,18 +1,25 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
 
-const [ , ,idx] = process.argv
+const [ , ,idx, ...items] = process.argv
 
-;( async function(idx){
-    
-    if(idx && !isNaN(idx)) {
-        idx = Number(idx);
+;( async function(idx, items){
+    if(items && idx && !isNaN(idx)) {
+        const newUser = items.join(' ');
         let users = await fs.readFile(path.join(__dirname, "users.json"), 'utf8');
+
         users = users ? JSON.parse(users) : [];
 
-        users.splice(idx, 1);
+        // id bo'yicha update qilish
+        // const index = users.findIndex(item => item.id == idx);
+        // if(index == -1) return console.log("ClientError: Wrong id inserted!");
+        // users.splice(index, 1, {id:Number(idx), name: newUser});
+
+        //index bo'yida update qilish
+        if(users.length <= idx) return console.log("ClientError: Wrong index inserted!");       
+        users[idx] = {id: users[idx].id, name: newUser};       
 
         fs.writeFile(path.join(__dirname, "users.json"), JSON.stringify(users, null, 4));
-        console.log("User successfully deleted");
+        console.log("User successfully updated");
     } else return console.log('Semething went wrong')
-})(idx);
+})(idx, items);
